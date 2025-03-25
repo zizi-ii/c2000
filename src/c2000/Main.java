dbbcb480e8dd425fb05aff9ad35cceccd6ecefe1
Lee, Jiwoo <zizii23027@gmail.com>
2025-03-25 21:31:01 +0900

    Main.java 수정 #4

--- src/c2000/Main.java
+++ src/c2000/Main.java
@@ -2,7 +2,12 @@ package c2000;
 
 public class Main {
 
+	static void say() {
+		System.out.println("main");
+	}
+
 	public static void main(String[] args) {
+		say();
 		Hello hello = new Hello();
 		hello.say("hello world");
 	}
